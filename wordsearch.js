ca78d7ad512d456b4d3377618a89a8b270bcb80f
Kabir Sewani <kabir.sewani@gmail.com>
2022-11-24 20:06:46 +0000

    fixed tests and implimented vertical search method using transpose function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    // Put your solution here
+
+    let result = [];
+    for (let x = 0; x < matrix[0].length; x++) {
+        const column = []
+        for (let y = 0; y < matrix.length; y++) {
+            column.push(matrix[0][x]);
+        }
+        result.push(column);
     }
+    return result;
+
+};
+
+
+
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+
+
+        for (l of horizontalJoin) {
+            if (l.includes(word)) {
+                return true
+            }
+        }
+
+        for (v of verticalJoin) {
+            if (v.includes(word)) {
+                return true
+            }
+        }
+    
+    return false
 }
+
 
 module.exports = wordSearch
